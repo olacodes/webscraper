9c19ca86584092512607b8870e8b9a7fe7d6e99a
Olatunde Sodiq <olatundesodiq@gmail.com>
2022-06-05 20:29:16 +0100

    add a simple ci test

--- webscraper/tests.py
+++ webscraper/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class TestCI(TestCase):
+
+    def test_continuous_integration(self):
+        self.assertTrue(True)
+
+    def test_continuous_integration_2(self):
+        self.assertEqual(1 + 1, 2)
